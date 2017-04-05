f465d13f44dca01d45ff783cbf122f6d42ff736a
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2017-04-05 09:53:38 +0530

    Added Working with submodules

--- example/src/script.js
+++ example/src/script.js
@@ -2,105 +2,89 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Prism from 'prismjs';
-// import bootstrap from 'bootstrap';
+import FusionTimeSeries from '../../src/FusionTimeSeries';
+import tsData from './data/datarepo';
 
 window.jQuery = $;
-
 window.bootstrap = require('bootstrap');
 window.ReactDOM = ReactDOM;
 
-
-var previous = "";
-
-
-/*** Onload ***/
-$('.navigation-tab').on('click', function() {
-    var url = window.location.href
-        , index = url.indexOf('/#')
-        , id = $(this)[0].id;
-    url = url.substr(index,url.length-index);
-    if(id === 'home-navigate') {
-        window.location.href = window.location.href.replace(url,'/#/home');
-    } else if(id === 'getting-started-navigate') {
-        window.location.href = window.location.href.replace(url,'/#/getting-started');
-    } else if(id === 'dashboard-navigate') {
-        window.location.href = window.location.href.replace(url,'/#/dashboard');
-        document.getElementById("dashboard-container").innerHTML='<object type="text/html" data="views/gtd-dashbaord-index.html" ></object>';
-    } else if(id === 'demos-navigate') {
-        window.location.href = window.location.href.replace(url,'/#/demos/ex1');
-        $('.examples#ex1').trigger('click');
-    }
-});
-
-
-$('.examples').on('click',function() {
-    var url = window.location.href
-        , index = url.indexOf('demos/')
-        , example = url.substr(index+6,(url.length - (index+6)))
-        , exampleId = this.id;
-    if(exampleId !== previous) {
-        document.getElementById("charts-example").innerHTML='<object class="chart-space" type="text/html" data="views/'+exampleId+'.html" ></object>';
-    }
-
-    $("#code-ex1").hide();
-    $("#code-ex2").hide();
-    $("#code-ex3").hide();
-    $("#code-ex4").hide();
-    $("#code-ex5").hide();
-
-    $("#code-"+exampleId).show();
-
-
-
-    // console.log(exampleId);
-    // jQuery.get({
-    //     url: "../src/js/" + exampleId + ".js",
-    //     // contentType: 'application/json',
-    //     dataType: "html",
-    //     success: function (data) {
-    //       console.log(data);
-    //         $('#code-javascript').html(data);
-    //         Prism.highlightAll();
-    //     }
-    // });
-
-    // $('#code-javascript').load('../src/js/'+exampleId+".js");
-
-    // jQuery.get({
-    //     url: "../src/views/" + exampleId + ".html",
-    //     // contentType: 'application/json',
-    //     dataType: 'html',
-    //     success: function (data) {
-    //         data = "\n" + data.replace(/\</g, '&lt;');
-    //         $('#html_code code.language-markup').html(data);
-    //         Prism.highlightAll();
-    //     }
-    // });
-
-    // $('#html_code code.language-markup').html(code[exampleId].html);
-    // $('#javascript_code code.language-javascript').html(code[exampleId].javascript);
-
-    previous = this.id;
-    Prism.highlightAll();
-});
-
-if(window.location.href.indexOf('#') === -1) {
-    window.location.href += '#/home';
-} else {
-    var url = window.location.href
-        , index = url.indexOf('demos/')
-        , example;
-    if(url.indexOf('home/') !== -1) {
-        $('.navigation-tab a[href="#home"]').tab('show')
-    } else if(url.indexOf('/dashboard') !== -1) {
-        $('.navigation-tab a[href="#dashboard"]').tab('show');
-        document.getElementById("dashboard-container").innerHTML='<object type="text/html" data="views/gtd-dashbaord-index.html" ></object>';
-    } else if(url.indexOf('/getting-started') !== -1) {
-        $('.navigation-tab a[href="#getting-started"]').tab('show');
-    } else if(index !== -1) {
-        $('.navigation-tab a[href="#demos"]').tab('show')
-        example = url.substr(index+6,(url.length - (index+6)));
-        $('.examples#'+example).trigger('click');
-    }
+const Snippet = () => {
+  return (
+    <pre>
+    <code className="language-javascript">
+    import React from 'react';
+    import ReactDOM from 'react-dom';
+    import FusionTimeSeries from 'FusionTimeSeries';
+    var fcDataSource = &#123;
+     // config goes here
+    &#125;
+    ReactDOM.render( &lt; FusionTimeSeries width = "850" height = "450" &#123; ...fcDataSource &#125; /&gt;, document.getElementById('chart-container'));
+    </code>
+    </pre>
+  );
 }
-Prism.highlightAll();
+
+class ListComponent extends React.Component {
+  constructor(props){
+    super(props);
+    this.charts = Object.keys(tsData);
+    this.tsData = tsData;
+    this.state = {
+      fcDataSource: {}
+    }
+  }
+  clickHandle(item) {
+
+    this.setState({
+      fcDataSource: this.tsData[item].data
+    })
+  }
+  componentWillMount() {
+    this.setState({
+      fcDataSource: this.tsData[this.charts[0]].data
+    })
+  }
+
+  render() {
+    const chartStyles = {
+      height: "450px",
+      minHeight: "450px"
+    };
+    return (
+      <div className="row">
+          <div className="col-md-3 tablist" >
+            <div className="list-group" role="tablist">
+            { this.charts.map((chart, i) => {
+                return (
+                  <span key={i} className="list-group-item examples" onClick={this.clickHandle.bind(this, chart)}>
+                    <h4>{this.tsData[chart].metadata.name}</h4>
+                    <p>{this.tsData[chart].metadata.desc}</p>
+                  </span>
+                );
+            })}
+            </div>
+          </div>
+        <div className="col-md-9" >
+            <div className="chartts">
+              <FusionTimeSeries width="850" height="450" { ...this.state.fcDataSource }/>
+            </div>
+          <div id="code-listing">
+          <ul className="nav nav-tabs">
+            <li className="active">
+              <a href="#javascript_code" data-toggle="tab">Javascript</a>
+            </li>
+          </ul>
+          <div className="tab-content">
+            <div id="javascript_code" className="tab-pane fade in active">
+            <Snippet />
+          </div>
+       </div>
+      </div>
+    </div>
+ </div>
+    );
+  }
+}
+
+ReactDOM.render(<ListComponent />, document.getElementById('demos'))
--- src/FusionTimeSeries.js
+++ src/FusionTimeSeries.js
@@ -2,13 +2,6 @@ import React, {
   Component
 }
 from 'react';
-//
-// require('./lib/fusioncharts');
-// require('./lib/fusioncharts.timeseries.js');
-// require('./lib/data-aggregator-es5.js');
-// require('./lib/growth-analyser-es5.js');
-// require('./lib/date-range-chooser-es5.js');
-// require('./lib/standard-period-selector-es5.js');
 
 import cloneDeep from 'lodash/cloneDeep';
 
@@ -16,47 +9,58 @@ import cloneDeep from 'lodash/cloneDeep';
 class FusionTimeSeries extends Component {
   constructor(props) {
     super(props);
-
-    this.dataSource = {};
-
-    this.dataSource.chart = cloneDeep(props.chart);
-    this.dataSource.extensions = cloneDeep(props.extensions);
-
+    this.chartConfig = {}
     this.chart = null;
+  }
 
-    this.chartConfig = {
-      type: "timeseries",
-			plottype: "line",
-	    renderAt: null ,
-			width: this.props.width,
-			height: this.props.height,
-			dataFormat: "json",
-			containerBackgroundOpacity: 0,
-      dataSource: cloneDeep(this.dataSource)
-    }
+  _setChartConfig(renderAt, attrs) {
+    this.chartConfig =  {
+        type: "timeseries",
+  			plottype: "line",
+  	    renderAt: renderAt ,
+  			width: attrs.width,
+  			height: attrs.height,
+  			dataFormat: "json",
+  			containerBackgroundOpacity: 0,
+        dataSource: {
+          chart: cloneDeep(attrs.chart),
+          extensions: cloneDeep(attrs.extensions)
+        }
+      };
+  }
+
+  _renderTsChart(){
+    this.chart = new FusionCharts(this.chartConfig);
+    this.chart.render();
   }
 
   componentDidMount() {
-    this.chartConfig.renderAt = this.refs.fcTimeChart;
-    console.log(this);
-    this.chart = new FusionCharts(this.chartConfig);
-    this.chart.render();
+    this._setChartConfig(this.refs.fcTimeChart, this.props);
+    if (!this.chart) {
+      this._renderTsChart();
+    }
   }
 
   componentWillUnmount() {
     this.chart && this.chart.dispose();
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps !== this.props) {
+      this._setChartConfig(this.refs.fcTimeChart, nextProps);
+      this._renderTsChart();
+    }
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps !== this.props) {
+      this._setChartConfig(this.refs.fcTimeChart, this.props);
+      this._renderTsChart();
+    }
   }
 
   render() {
-
-    return (< div
-        ref="fcTimeChart"
-      />
-    );
+    return (< div ref="fcTimeChart"/>);
   }
 }
 
