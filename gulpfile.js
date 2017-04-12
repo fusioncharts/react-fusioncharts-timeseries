c18ff6a28834c34c6e9bef9677a81a17518c850c
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2017-04-12 16:08:46 +0530

    Added minify script

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,48 @@
-let gulp = require('gulp'),
-  initGulpTasks = require('react-component-gulp-tasks'),
+const gulp = require('gulp');
+const initGulpTasks = require('react-component-gulp-tasks');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
 
-  /**
-   * Tasks are added by the react-component-gulp-tasks package
-   *
-   * See https://github.com/JedWatson/react-component-gulp-tasks
-   * for documentation.
-   *
-   * You can also add your own additional gulp tasks if you like.
-   */
+const taskConfig = {
+  component: {
+    name: 'FusionTime',
+    dependencies: [
+      'classnames',
+      'react',
+      'react-dom',
+      'fusioncharts',
+    ],
+    lib: 'lib',
+  },
 
-  taskConfig = {
-    component: {
-      name: 'FusionTime',
-      dependencies: [
-        'classnames',
-        'react',
-        'react-dom',
-        'fusioncharts',
-      ],
-      lib: 'lib',
-    },
-
-    example: {
-      src: 'example/src',
-      dist: 'example/dist',
-      files: [
-        'index.html',
-        'url-sample.html',
-        './images/*.png',
-        './data/*',
-        './views/*.html',
+  example: {
+    src: 'example/src',
+    dist: 'example/dist',
+    files: [
+      'index.html',
+      'url-sample.html',
+      './images/*.png',
+      './data/*',
+      './views/*.html',
         // '../fonts/*',
-        './css/*.css',
-        '.gitignore',
-      ],
-      scripts: [
-        'script.js',
-      ],
-      less: [
-        './css/style.less',
-      ],
-    },
-  };
+      './css/*.css',
+      '.gitignore',
+    ],
+    scripts: [
+      'script.js',
+    ],
+    less: [
+      './css/style.less',
+    ],
+  },
+};
 
 initGulpTasks(gulp, taskConfig);
+
+gulp.task('compress', () => {
+  pump([
+    gulp.src('lib/*.js'),
+    uglify(),
+    gulp.dest('lib'),
+  ]);
+});
